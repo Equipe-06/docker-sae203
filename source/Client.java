3f1ef941e0768b6a4d9a8b90f87860e9cbb81e1b
Ethan Damestoy <cytelid@gmail.com>
2025-04-29 15:44:42 +0200

    Update Client.java

--- source/Client.java
+++ source/Client.java
@@ -8,10 +8,11 @@ import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Client 
 {
-    private static final String SERVER_IP   = "5.tcp.eu.ngrok.io";
-    private static final int    SERVER_PORT = 12108;
+    private static final String SERVER_IP   = "localhost";
+    private static final int    SERVER_PORT = 9000;
 
     private FrameClient frameClient;
 
@@ -28,14 +29,17 @@ public class Client
     {
         try 
         {
+            // Connexion au serveur
             System.out.println("Connexion au serveur " + SERVER_IP + ":" + SERVER_PORT + "...");
             Socket socket = new Socket(SERVER_IP, SERVER_PORT);
             System.out.println("Connecté au serveur!");
 
+            // Flux d'entrée/sortie
             this.out        = new PrintWriter(socket.getOutputStream(), true);
             this.in         = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scanner = new Scanner(System.in);
-
+            
+            // Thread pour lire les messages du serveur ( Obligatoire si on veut pas bloquer le terminal )
             Thread receiveThread = new Thread(() -> {
                 try 
                 {
@@ -51,7 +55,8 @@ public class Client
                 catch (IOException e) { System.out.println("Erreur réception: " + e.getMessage()); }
             });
             receiveThread.start();
-
+                
+            // Boucle principale pour envoyer des messages
             while (true) 
             {
                 String userInput = scanner.nextLine();
